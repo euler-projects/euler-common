d3ff57275e26089530191bb417319cf845b7b804
cFrost <cFrost@eulerproject.io>
2017-01-21 11:10:49 +0800

    change class name

--- src/main/java/net/eulerframework/common/util/GlobalProperties.java
+++ src/main/java/net/eulerframework/common/util/GlobalProperties.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class GlobalProperties1 {
+public class GlobalProperties {
     
     protected final static Logger logger = LogManager.getLogger();
 	
