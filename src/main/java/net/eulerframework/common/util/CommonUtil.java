30f28dde0274d5b1aa2ce0e0eef2d576baea711b
cFrost <cFrost@eulerproject.io>
2017-02-10 18:42:49 +0800

    refact code

--- src/main/java/net/eulerframework/common/util/CommonUtil.java
+++ src/main/java/net/eulerframework/common/util/CommonUtil.java
@@ -2,6 +2,10 @@ package net.eulerframework.common.util;
 
 public class CommonUtil {
 
+    /**
+     * 程序暂停
+     * @param seconds 暂停秒数
+     */
     public static void sleep(int seconds) {
         int i = 0;
         while(i++ < seconds) {
@@ -12,5 +16,28 @@ public class CommonUtil {
             }
             System.out.println("Sleep... "+ i +"s");
         }
+    }
+
+    /**
+     * 统一路径为UNIX格式,结尾的"/"会去掉,如果只有一个"/"，则会保留<br>
+     * Examples:
+     * <pre>
+     * convertDirToUnixFormat("\") returns "/"
+     * convertDirToUnixFormat("D:\floder\") returns "D:/floder"
+     * convertDirToUnixFormat("D:\floder\file") returns "D:/floder/file"
+     * </pre>
+     * 
+     * @param dir 原始路径
+     * @return unix路径
+     */
+    public static String convertDirToUnixFormat(String dir) {
+        if (dir == null)
+            return dir;
+    
+        String unixDir = dir.replace("\\", "/");
+        if (unixDir.endsWith("/") && unixDir.length() > 1) {
+            unixDir = unixDir.substring(0, unixDir.length() - 1);
+        }
+        return unixDir;
     }
 }
