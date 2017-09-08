597ad29c890d4d17df5bc7638d5c0f105ca1cb8a
cfrost <cfrost@eulerproject.io>
2017-09-09 02:07:17 +0800

    删除无用代码

--- euler-common-util/src/test/java/net/eulerframework/common/ArrayTest.java
+++ euler-common-util/src/test/java/net/eulerframework/common/ArrayTest.java
@@ -1,20 +1,15 @@
 package net.eulerframework.common;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 
 import org.junit.Test;
 
 import net.eulerframework.common.util.io.file.FileReadException;
-import net.eulerframework.common.util.io.file.SimpleFileIOUtils;
 
 public class ArrayTest {
 
     @Test
     public void voidArrayTest() throws FileNotFoundException, FileReadException {
-        File file = new File("D:\\Users\\cFrost\\Downloads\\a.torrent");
-        for(int i = 0; i < 10000; i++) {
-            System.out.println(SimpleFileIOUtils.readFileByByte(file).length);            
-        }
+        
     }
 }
