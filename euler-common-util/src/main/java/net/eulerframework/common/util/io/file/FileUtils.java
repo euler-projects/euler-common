d01267d6e3f740067ce56eff1ffe8cfd891a04db
cfrost <cfrost@eulerproject.io>
2017-11-26 23:56:55 +0800

    fix license header

--- euler-common-util/src/main/java/net/eulerframework/common/util/io/file/FileUtils.java
+++ euler-common-util/src/main/java/net/eulerframework/common/util/io/file/FileUtils.java
@@ -24,7 +24,6 @@
  * For more information, please visit the following website
  * 
  * https://eulerproject.io
- * https://github.com/euler-form/web-form
  * https://cfrost.net
  */
 package net.eulerframework.common.util.io.file;
