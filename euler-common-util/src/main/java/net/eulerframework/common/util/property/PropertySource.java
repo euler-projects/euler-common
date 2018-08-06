c76468a3213d7cd402ece869d7581d5b8f6267ec
cFrost <cFrost@eulerproject.io>
2018-08-07 00:00:37 +0800

    Properties file reading will be aborted without exception if the file does not exist.

--- euler-common-util/src/main/java/net/eulerframework/common/util/property/PropertySource.java
+++ euler-common-util/src/main/java/net/eulerframework/common/util/property/PropertySource.java
@@ -1,6 +1,34 @@
+/*
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2013-2018 Euler Project 
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * For more information, please visit the following website
+ * 
+ * https://eulerproject.io
+ */
 package net.eulerframework.common.util.property;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -46,6 +74,8 @@ public class PropertySource extends LogSupport {
             this.logger.info("Load property file: " + uri);
             bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             this.props.load(bufferedReader);
+        } catch (FileNotFoundException e1) {
+            this.logger.warn("Property file " + uri + " does not exist, abord.");
         } finally {
             if(inputStream != null)
                 inputStream.close();
