fac308f0ec970cae9bbebb5c7b79b86fa630057c
cFrost <cFrost@eulerproject.io>
2025-01-02 16:26:49 +0800

    length mode

--- euler-common-proto/src/main/java/org/eulerframework/proto/annotation/LengthMode.java
+++
@@ -1,8 +0,0 @@
-package org.eulerframework.proto.annotation;
-
-public class LengthMode {
-    public static final String FIXED = "FIXED";
-    public static final String PREFIX = "PREFIX";
-    public static final String SEPARATOR = "SEPARATOR";
-    public static final String ALL_BYTES = "ALL_BYTES";
-}
--- euler-common-proto/src/main/java/org/eulerframework/proto/annotation/StringProperty.java
+++ euler-common-proto/src/main/java/org/eulerframework/proto/annotation/StringProperty.java
@@ -1,9 +1,15 @@
 package org.eulerframework.proto.annotation;
 
 public @interface StringProperty {
-    String lengthMode() default LengthMode.FIXED;
+
+    String lengthMode() default LENGTH_MODE_FIXED;
 
     String lengthPrefixType() default "";
 
     byte[] separator() default {};
+
+    String LENGTH_MODE_FIXED = "FIXED";
+    String LENGTH_MODE_PREFIX = "PREFIX";
+    String LENGTH_MODE_SEPARATOR = "SEPARATOR";
+    String LENGTH_MODE_ALL_BYTES = "ALL_BYTES";
 }
